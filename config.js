6f0bb3c69bddb3f49d6e64c13e7e7f6b6f701b7c
HarshYogi <110062864+HarshYogi@users.noreply.github.com>
2023-11-01 22:45:10 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Happy 3rd Anniversary",
+    introTitle: 'Hello Babeee',
+    introDesc: `Earth is inherently strange
+    But why do I keep going the wrong way?
+    Lost in my lonely heart
+    Behind the word love is love`,
+    btnIntro: `Let's Go`,
+    title: 'Wishing us a joyous and unforgettable 3rd anniversary, my love. 🥰',
+    desc: `Will you promise to be mine until our last breath, even if that means we're both competing to see who can hold their breath the longest?`,
+    btnYes: 'Ofc Yes <33',
+    btnNo: 'Maybe :3',
+    question: `What's the secret behind your decision to stick with me for three amazing years? <3`,
+    btnReply: 'Sent to you <3',
+    mess: `Thank you for being my heart's constant companion 😘😘`,
+    messDesc: 'Your presence has turned these past three years into the most enchanting and beautiful chapter of my life.',
+    btnAccept: 'Ta Ta betu <3',
+    messLink: 'https://anniversary.netlify.app'
+}
